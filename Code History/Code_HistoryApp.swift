be2446891750e1baf22f914211b484213132e523
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-05-02 09:37:50 +0930

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Matthew Morrison on 2/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
